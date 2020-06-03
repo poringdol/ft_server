565f759ae65d2d2035fa1ba5c0337e0dd069c27b
CDdoma <sashe@bk.ru>
2020-06-03 12:57:27 +0300

    fix

--- srcs/run.sh
+++ srcs/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # start services
-service nginx start
 service php7.3-fpm start
 service mysql start
 
@@ -13,10 +12,13 @@ mariadb -e "GRANT ALL PRIVILEGES ON db_wordpress.* TO 'pdemocri'@'localhost';"
 # import database with site
 mysql --user='pdemocri' --password='pdemocri' db_wordpress < '/var/lib/mysql/localhost.sql'
 
-if [ ! -f .initialized ]; then                                                                                                                                                                                    
-    echo "Initializing container"                                                                                                                                                                                 
-    # run initializing commands                                                                                                                                                                                   
-    touch .initialized                                                                                                                                                                                            
-fi                                                                                                                                                                                                                
-
+# for interactive mode (-it)  1st string, for daemon mode (-d) second string
+service nginx start
+# nginx -g 'daemon off;'
+ 
+if [ ! -f .initialized ]; then
+    echo "Initializing container"
+    # run initializing commands
+    touch .initialized
+fi
 exec "$@"
